a54cf21971d608387dce2e3c0c2ee979e64eff91
kimcho <hins110@163.com>
2018-02-11 13:39:29 +0800

    fix #5 perfect config.py file

---
+++ jobplus/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from jobplus.config import configs
+
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(configs.get(config))
+    # db.init_app(app)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
